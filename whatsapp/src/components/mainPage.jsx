12c140756f0e0c2fb75c38bd4a8a9764063b7582
LeoToti <Londra27?>
2021-07-19 14:39:30 +0200

    login

--- whatsapp/src/components/mainPage.jsx
+++ whatsapp/src/components/mainPage.jsx
@@ -2,18 +2,24 @@ import React from "react";
 
 import {Container, Row, Col} from "react-bootstrap"
 import ChatItems from "./ChatItems";
+import Search from "./Search"
+import TopLeftBar from "./TopLeftBar";
+import Login from "./Login";
 
 export default function MainPage() {
     return (
         <Container>
             <Row>
                 <Col>
+                <TopLeftBar/>
+                <Search/>
                     <ChatItems />
                 </Col>
                 <Col>
                     <chatPage />
                 </Col>
             </Row>
+            <Login/>
         </Container>
     );
 }
