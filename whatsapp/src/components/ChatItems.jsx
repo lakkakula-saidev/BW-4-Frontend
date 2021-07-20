0d89fd8651e2a4f5b1d5a3cbc4aafe8d010a1a68
Saidev Lakkakula <lakkakula.saidev@gmail.com>
2021-07-20 23:45:57 +0200

    sdfdf

--- whatsapp/src/components/ChatItems.jsx
+++ whatsapp/src/components/ChatItems.jsx
@@ -1,22 +1,20 @@
-import { ChatList } from 'react-chat-elements'
-import 'react-chat-elements/dist/main.css';
+import { ChatList } from "react-chat-elements";
+import "react-chat-elements/dist/main.css";
 
-
-
-    export default function ChatItems() {
-        return (
-            <ChatList
-            className='chat-list'
+export default function ChatItems() {
+    return (
+        <ChatList
+            className="chat-list"
             dataSource={[
                 {
-                    avatar: 'https://facebook.github.io/react/img/logo.svg',
-                    alt: 'Reactjs',
-                    title: 'Facebook',
-                    subtitle: 'What are you doing?',
+                    avatar: "https://source.unsplash.com/random",
+                    alt: "Reactjs",
+                    title: "Facebook",
+                    subtitle: "What are you doing?",
                     date: new Date(),
-                    unread: 0,
-                },
-               
-            ]} />
-        );
-    }
+                    unread: 0
+                }
+            ]}
+        />
+    );
+}
